5bb60c2cec9ab0b793fe8f7c9806b4d03c15535a
Monarch <64804940+anima-regem@users.noreply.github.com>
2022-10-11 02:28:33 +0530

    LoginPage x Tailwind

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -4,7 +4,7 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 const SignIn = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
-    var email = document.getElementById("email").value;
+    var email = document.getElementById("email-address").value;
     var password = document.getElementById("password").value;
     try {
       signInWithEmailAndPassword(auth, email, password);
@@ -13,13 +13,56 @@ const SignIn = () => {
     }
   };
   return (
-    <div>
-      <h1>Sign In</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="email" placeholder="Email" id="email" />
-        <input type="password" placeholder="Password" id="password" />
-        <button type="submit">Sign In</button>
-      </form>
+
+    <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div class="w-full max-w-md space-y-8">
+        <div>
+          <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+            Sign In
+          </h2>
+        </div>
+        <form class="mt-8 space-y-6" onSubmit={handleSubmit}>
+          <div class="-space-y-px rounded-md shadow-sm">
+            <div>
+              <label for="email-address" class="sr-only">
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autocomplete="off"
+                required
+                class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                placeholder="Email address"
+              />
+            </div>
+            <div>
+              <label for="password" class="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autocomplete="current-password"
+                required
+                class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
